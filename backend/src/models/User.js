67666ca1c45bdadd2a731fc3160609938ed60f99
hyorimlee <hl1046@pusan.ac.kr>
2022-04-26 16:56:50 +0900

    [S06P31A302-127] feat: fix schema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,26 +1,25 @@
 const { Schema, model } = require('mongoose')
-const { ItemSchema } = require('./Item')
-const { CollectionSchema } = require('./Collection')
 
 const UserSchema = new Schema({
     username: { type: String, required: true , unique: true },
     password: { type: String, required: true },
-
-    // phone 인증번호 doc을 가져올 수도 있음
-    phone: { type: String, required: true },
-
     nickname: String,
-    image: String,
-    description: String,
-    following: [UserSchema],
-    follower: [UserSchema],
-    
-    // Chat도 Collection의 Hashtags와 마찬가지
-    // chat: []
-
-    items: [ItemSchema],
-    collections: [CollectionSchema],
-
+    // phone: {
+    //     type:Schema.Types.String,
+    //     ref:'phone',
+    // },
+    // follow: {
+    //     type:Schema.Types.ObjectId,
+    //     ref:'follow',
+    // },
+    // profile: {
+    //     type:Schema.Types.String,
+    //     ref:'profile',
+    // },
+    // review: {
+    //     type:Schema.Types.String,
+    //     ref:'review',
+    // },
 }, { timestamps: true })
 
 const User = model('user', UserSchema)
