30259f302896c3208e894ec765f5b2217259a98e
itzmeee <itzmeee@naver.com>
2022-04-29 10:18:35 +0900

    [S06P31A302-132] update: Get collections list

--- backend/src/models/Collection.js
+++ backend/src/models/Collection.js
@@ -1,15 +1,15 @@
 const { Schema, model, Types: { ObjectId } } = require('mongoose')
 
 const CollectionSchema = new Schema(
-    {
-        title: { type: String , required: true },
-        description: { type: String, required: false },
-        items: [ItemSchema],
-        like_users: [UserSchema],
-        user: { type: ObjectId, required: true },
-        isPublic: { type: Boolean, required: true }
-    },
-    { timestamps: true })
+  {
+    title: { type: String , required: true },
+    description: { type: String, required: false },
+    items: [ItemSchema],
+    like_users: [UserSchema],
+    user: { type: ObjectId, required: true },
+    isPublic: { type: Boolean, required: true }
+  },
+  { timestamps: true })
 
 const Collection = model('collection', CollectionSchema)
 
--- backend/src/routes/collection.js
+++ backend/src/routes/collection.js
@@ -5,78 +5,79 @@ const { isValidObjectId } = require('mongoose')
 const { User, Collection, Profile } = require('../models')
 
 collectionRouter.post('/', async (req, res) => {
-    try {
-        // jwt 검증: user 추출 및 검증
-        // if (!isValidObjectId(userId)) return res.status(400).send({ err: "invalid userId" })
-        // const user = await User.findById(userId)
+  try {
+    // jwt 검증: user 추출 및 검증
+    // if (!isValidObjectId(userId)) return res.status(400).send({ err: "invalid userId" })
+    // const user = await User.findById(userId)
 
-        // title, description, isPublic 추출 및 검증
-        const { title, description, isPublic } = req.body
-        if (typeof title !== 'string') return res.status(400).send({ err: "string title is required"});
-        if (description && typeof description !== 'string') return res.status(400).send({ err: "description must be string type"});
-        if (typeof isPublic !== 'boolean') return res.status(400).send({ err: "boolean isPublic is required"});
+    // title, description, isPublic 추출 및 검증
+    const { title, description, isPublic } = req.body
+    if (typeof title !== 'string') return res.status(400).send({ err: "string title is required"});
+    if (description && typeof description !== 'string') return res.status(400).send({ err: "description must be string type"});
+    if (typeof isPublic !== 'boolean') return res.status(400).send({ err: "boolean isPublic is required"});
 
-        // 컬렉션 자체 추가 & 프로필의 컬렉션 목록에 추가
-        const collection = new Collection({ ...req.body, user })
-        const profileId = await User.findById(userId).profile
-        await Promise.all([
-          collection.save(),
-          Profile.updateOne({ _id: profileId }, { $push: { collections: collection }})
-        ])
-        return res.status(201).send({ collection })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send({ err: error.message })
-    }
+    // 컬렉션 자체 추가 & 프로필의 컬렉션 목록에 추가
+    const collection = new Collection({ ...req.body, user })
+    const profileId = await User.findById(userId).profile
+    await Promise.all([
+      collection.save(),
+      Profile.updateOne({ _id: profileId }, { $push: { collections: collection }})
+    ])
+    return res.status(201).send({ collection })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({ err: error.message })
+  }
 })
 
 collectionRouter.get('/:userId', async (req, res) => {
-    try {
-        const { userId } = req.params
-        // const user = await User.findById(userId)
-        // 컬렉션 목록 조회 w/ pagination. 최신 업데이트 순. page는 1부터 시작. 3개씩 조회.
-        const collections = await Collection.find({ user: userId }).sort({ updatedAt: -1 }).skip((page - 1) * 3).limit(3)
-        return res.status(200).send({ collections })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send({ err: error.message })
-    }
+  try {
+    const { userId } = req.params
+    if (!isValidObjectId(userId)) return res.status(400).send({ err: "invalid userId"})
+    const user = await User.findById(userId)
+    // 컬렉션 목록 조회 w/ pagination. 최신 업데이트 순. page는 1부터 시작. 3개씩 조회.
+    const collections = await Collection.find({ user: userId }).sort({ updatedAt: -1 }).skip((page - 1) * 3).limit(3)
+    return res.status(200).send({ collections })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({ err: error.message })
+  }
 })
 
 collectionRouter.get('/:collectionId', async (req, res) => {
-    try {
-        const collection = null
-        // 컬렉션 상세 조회
+  try {
+    const collection = null
+    // 컬렉션 상세 조회
 
-        return res.status(200).send({ collection })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send({ err: error.message })
-    }
+    return res.status(200).send({ collection })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({ err: error.message })
+  }
 })
 
 
 collectionRouter.patch('/:collectionId', async (req, res) => {
-    try {
-        const collection = null
-        // 프로필 수정 로직
+  try {
+    const collection = null
+    // 프로필 수정 로직
 
-        return res.status(200).send({ collection })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send({ err: error.message })
-    }
+    return res.status(200).send({ collection })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({ err: error.message })
+  }
 })
 
 collectionRouter.delete('/:collectionId', async (req, res) => {
-    try {
-        const { collectionId } = req.params
-        const collection = await Collection.findByIdAndDelete(collectionId)
-        return res.status(200).send({ collection })
-    } catch (error) {
-        console.log(error)
-        return res.status(500).send({ err: error.message })
-    }
+  try {
+    const { collectionId } = req.params
+    const collection = await Collection.findByIdAndDelete(collectionId)
+    return res.status(200).send({ collection })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({ err: error.message })
+  }
 })
 
 module.exports = {
