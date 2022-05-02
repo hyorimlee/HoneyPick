bdb0d773f1aae65f540c57beb0c2a4440e333955
sosin <svstar94@naver.com>
2022-05-02 18:26:32 +0900

    [S06P31A302-158] add: review api

---
+++ backend/src/models/Review.js
@@ -0,0 +1,14 @@
+const { Schema, model, Types: { ObjectId } } = require('mongoose')
+
+const ReviewSchema = new Schema({
+    user: { type: ObjectId, required: true, ref: 'user' },
+    item: { type: ObjectId, required: true, ref: 'item' },
+    isRecommend: { type: Number, required: true },
+    stickers: { type: Array, required: true, default: []}
+}, { timestamps: true })
+
+// index - multiIndex(user, item)
+
+const Review = model('review', ReviewSchema)
+
+module.exports = { Review }
