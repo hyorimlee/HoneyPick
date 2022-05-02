3816b036c8b76380e03946d24cb1dfcf403f99c0
yoon <jiyoonhan12@gmail.com>
2022-05-02 23:38:20 +0900

    [S06P31A302-151] feat : item page save

--- frontend/src/containers/recommendBar/index.tsx
+++ frontend/src/containers/recommendBar/index.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
 import {memo} from 'react'
-import {View} from 'react-native'
 import GaugeBar from '../../components/gaugeBar'
+import {Container} from './styles'
 
 function RecommendBar() {
   return (
-    <View>
+    <Container>
       <GaugeBar emoji='🎁' text='선물하기 좋아요' votes={222}></GaugeBar>
       <GaugeBar emoji='👔' text='재질이 부드러워요' votes={13}></GaugeBar>
       <GaugeBar emoji='⛏' text='내구성이 좋아요' votes={5}></GaugeBar>
-    </View>
+    </Container>
   )
 }
 
--- frontend/src/pages/item/index.tsx
+++ frontend/src/pages/item/index.tsx
@@ -1,33 +1,53 @@
 import * as React from 'react'
 import {memo} from 'react'
-import {View, Text, Image} from 'react-native'
+import {Alert, SafeAreaView, StatusBar} from 'react-native'
+import {Container, ImageContainer, MenuContainer, TextContainer, NormalText, BoldText, PriceText, DashedBorder} from './styles'
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome'
+import {faEllipsisVertical} from '@fortawesome/free-solid-svg-icons'
 import RecommendBar from '../../containers/recommendBar'
-import {Container, ImageContainer, PriceText, DashedBorder} from './styles'
+import BaseButton from '../../components/button/base'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
 
 function Item() {
+
+  // 사이트로 이동하기
+  const goToSite = () => {
+    Alert.alert('임시~~~')
+  }
+
   return (
-    <Container>
-      <ImageContainer
-        source={require('../../assets/images/sampleimage1.jpg')}
-        imageStyle={{
-          resizeMode: 'contain',
-          borderRadius: 20,
-        }}
-      />
-      <Text>사이트명</Text>
-      <Text>아이템 이름</Text>
-      <PriceText>가격</PriceText>
-      <Text>컬렉션 이름</Text>
-      <View style={{
-        borderStyle: 'dashed',
-        borderBottomWidth: 1,
-      }}>
-      </View>
-      {/* dashedborder 안먹는 중 */}
-      <DashedBorder />
-      <Text>다른 허니비들이 이 아이템을 추천하는 이유</Text>
-      <RecommendBar></RecommendBar>
-    </Container>
+    <SafeAreaView style={{flex: 1, paddingTop: StatusBar.currentHeight}}>
+      <Container>
+        <ImageContainer
+          source={require('../../assets/images/sampleimage1.jpg')}
+          imageStyle={{
+            resizeMode: 'contain',
+            borderRadius: 20,
+          }}
+        />
+        <MenuContainer>
+          <TextContainer>
+            <NormalText>사이트명</NormalText>
+            <BoldText>아이템 이름</BoldText>
+            <PriceText>가격</PriceText>
+            <NormalText>컬렉션 이름</NormalText>
+          </TextContainer>
+          <FontAwesomeIcon icon={faEllipsisVertical as IconProp} color='#C4C4C4' size={24} style={{marginTop: 15}} />
+        </MenuContainer>
+        <DashedBorder />
+        <TextContainer>
+          <NormalText>다른 허니비들이 이 아이템을 추천하는 이유</NormalText>
+          <RecommendBar></RecommendBar>
+        </TextContainer>
+        <BaseButton
+          text={'사이트로 이동하기'}
+          onPress={goToSite}
+          borderRadius={25}
+          marginVertical={10}
+          paddingVertical={15}
+        />
+      </Container>
+    </SafeAreaView>
   )
 }
 
