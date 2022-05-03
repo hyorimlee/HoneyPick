d3d58ae37b11892fc2a5af4ab29c2fb74e4d5d2a
sosin <svstar94@naver.com>
2022-05-03 14:49:01 +0900

    [S06P31A302-148] add: verify phone number

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,4 +4,5 @@ router.use('/auth', require('./auth'))
 router.use('/item', require('./item'))
 router.use('/collection', require('./collection'))
 router.use('/follow', require('./follow'))
+router.use('/phone', require('./phone'))
 module.exports = router
