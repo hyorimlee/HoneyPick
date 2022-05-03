e817f4a6b870348c11fbb8a3f353780303b9f84e
itzmeee <itzmeee@naver.com>
2022-05-03 11:51:04 +0900

    [S06P31A302-150] update: complemented source code

--- backend/src/routes/collection.js
+++ backend/src/routes/collection.js
@@ -1,16 +1,25 @@
 const { Router } = require('express')
 const collectionRouter = Router()
-const mongoose = require('mongoose')
 const { isValidObjectId } = require('mongoose')
 const { User, Collection, Profile, Item } = require('../models')
 const { authAccessToken } = require('./auth')
+
+// 팔로워인지 검증
+function isFollower(accountId, userId) {
+  const profileId = await User.findById(accountId).profile
+  const followers = await Profile.findById(profileId).followers
+  if (followers.includes(userId)) {
+    return true
+  }
+  return false
+}
 
 // 컬렉션 생성
 collectionRouter.post('/', async (req, res) => {
   try {
     // jwt 검증: user 추출 및 검증
     const userId = await authAccessToken(req, res)
-    // if (!isValidObjectId(userId)) return res.status(400).send({ err: "invalid userId" })
+    if (!isValidObjectId(userId)) return res.status(400).send({ err: "invalid userId" })
     const user = await User.findById(userId)
 
     // title, description, isPublic 추출 및 검증
@@ -34,22 +43,25 @@ collectionRouter.post('/', async (req, res) => {
 })
 
 // 컬렉션 목록 조회
-collectionRouter.get('/:userId', async (req, res) => {
+collectionRouter.get('/:accountId', async (req, res) => {
   try {
-    const { userId } = req.params
     let { page=1 } = req.query
     page = parseInt(page)
-    if (!isValidObjectId(userId)) return res.status(400).send({ err: "invalid userId"})
-    const user = await User.findById(userId)
+    const { accountId } = req.params
+    if (!isValidObjectId(accountId)) return res.status(400).send({ err: "invalid accountId"})
 
-    // 비공개인 컬렉션은, 사용자가 팔로워여야만 조회 가능
-    const accessingId = await authAccessToken(req, res)
+    // 비공개인 컬렉션은, 사용자가 팔로워여야만 조회 가능 (jwt)
+    const userId = await authAccessToken(req, res)
+    if (!isValidObjectId(userId)) return res.status(400).send({ err: "invalid userId" })
     // 팔로워 목록 조회해서, 팔로워면 all, 아니면 public 보여주기
-
     // 컬렉션 목록 조회 w/ pagination. 최신 업데이트 순. page는 1부터 시작. 3개씩 조회.
-    const publicCollections = await Collection.find({ user: userId, isPublic: true }).sort({ updatedAt: -1 }).skip((page - 1) * 3).limit(3)
-    const allCollections = await Collection.find({ user: userId }).sort({ updatedAt: -1 }).skip((page - 1) * 3).limit(3)
-    return res.status(200).send({ collections })
+    if (isFollower(accountId, userId)) {
+      const allCollections = await Collection.find({ user: userId }).sort({ updatedAt: -1 }).skip((page - 1) * 3).limit(3)
+      return res.status(200).send({ collections: allCollections })
+    } else {
+      const publicCollections = await Collection.find({ user: userId, isPublic: true }).sort({ updatedAt: -1 }).skip((page - 1) * 3).limit(3)
+      return res.status(200).send({ collections: publicCollections })
+    }
   } catch (error) {
     console.log(error)
     return res.status(500).send({ err: error.message })
@@ -57,11 +69,21 @@ collectionRouter.get('/:userId', async (req, res) => {
 })
 
 // 컬렉션 상세 조회
-collectionRouter.get('/:userId/:collectionId', async (req, res) => {
+collectionRouter.get('/:accountId/:collectionId', async (req, res) => {
   try {
-    // 비공개인 경우: jwt 토큰에서 사용자Id 가져와서 userId 의 팔로워 목록에 있는지 확인하고, 있으면 공개, 없으면 못 봄
-    const { collectionId } = req.params
+    const { accountId, collectionId } = req.params
+    if (!isValidObjectId(accountId)) return res.status(400).send({ err: "invalid accountId" })
+    if (!isValidObjectId(collectionId)) return res.status(400).send({ err: "invalid collectionId" })
+
+    // 비공개인 경우: jwt 토큰에서 userId 가져와서 accountId 의 팔로워 목록에 있는지 확인하고, 있으면 공개, 없으면 못 봄
     const collection = await Collection.findById(collectionId)
+    if (collection.isPublic == 'false') {
+      const userId = await authAccessToken(req, res)
+      if (!isValidObjectId(userId)) return res.status(400).send({ err: "invalid userId" })
+      if (!isFollower(accountId, userId)) {
+        return res.status(400).send({ err: 'private collection'})
+      }
+    }
     return res.status(200).send({ collection })
   } catch (error) {
     console.log(error)
@@ -70,7 +92,7 @@ collectionRouter.get('/:userId/:collectionId', async (req, res) => {
 })
 
 // 컬렉션 수정(제목, 설명, 아이템, 공개여부)
-collectionRouter.patch('/:userId/:collectionId', async (req, res) => {
+collectionRouter.patch('/:accountId/:collectionId', async (req, res) => {
   try {
     const { collectionId } = req.params
     const { title, description, itemId, isPublic } = req.body
@@ -102,13 +124,13 @@ collectionRouter.patch('/:userId/:collectionId', async (req, res) => {
 })
 
 // 컬렉션 삭제
-collectionRouter.delete('/:userId/:collectionId', async (req, res) => {
+collectionRouter.delete('/:accountId/:collectionId', async (req, res) => {
   try {
-    const { userId, collectionId } = req.params
-    if (!isValidObjectId(userId)) return res.status(400).send({ err: "invalid userId"})
+    const { accountId, collectionId } = req.params
+    if (!isValidObjectId(accountId)) return res.status(400).send({ err: "invalid accountId"})
     if (!isValidObjectId(collectionId)) return res.status(400).send({ err: "invalid collectionId"})
 
-    const profileId = await User.findById(userId).profile
+    const profileId = await User.findById(accountId).profile
     // 컬렉션 자체 삭제
     const collection = await Collection.findByIdAndDelete(collectionId)
     // 프로필의 컬렉션 목록에서 삭제
