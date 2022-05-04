980925ac9b3b3080449d781f5c51b7c4ad0a6737
unho-00 <djsgh94@naver.com>
2022-05-04 15:42:54 +0900

    [S06P31A302-188] feat : accessToken auto refresh

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -5,66 +5,76 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import EncryptedStorage from 'react-native-encrypted-storage'
 import store from './src/store'
-import {useAppDispatch, useAppSelector} from './src/store/types'
+import {RootState, useAppDispatch, useAppSelector} from './src/store/types'
 import SignIn from './src/pages/signIn'
 import SignUp from './src/pages/signUp'
 import ProfileStack from './src/pages/profile'
 import SplashScreen from 'react-native-splash-screen'
 import {requestAccessToken} from './src/store/slices/user/asyncThunk'
 import axios from 'axios'
+import {
+  AnyAction,
+  CombinedState,
+  Dispatch,
+  ThunkDispatch,
+} from '@reduxjs/toolkit'
 
 const Tab = createBottomTabNavigator()
 const Stack = createNativeStackNavigator()
+
+type IDispatch = ThunkDispatch<CombinedState<RootState>, undefined, AnyAction> &
+  Dispatch
+
+// 스플래시 스크린 시간동안 EncryptedStorage 에 저장된 refreshToken 을 이용하여 accessToken 발급받기
+const getRefreshToken = async (dispatch: IDispatch) => {
+  const refreshToken = await EncryptedStorage.getItem('refreshToken')
+  if (!refreshToken) {
+    SplashScreen.hide()
+    return
+  }
+
+  dispatch(requestAccessToken({refreshToken}))
+    .unwrap()
+    .then(() => {
+      SplashScreen.hide()
+    })
+    .catch(() => {
+      SplashScreen.hide()
+    })
+}
+
+// axios interceptor 정의
+const axiosInterceptor = (dispatch: IDispatch) => {
+  axios.interceptors.response.use(
+    response => response,
+    async error => {
+      const {
+        config,
+        response: {status},
+      } = error
+
+      // access token 기간 만료시
+      if (status === 419) {
+        console.log('accessToken 기간만료! - accessToken 재요청')
+        const refreshToken = await EncryptedStorage.getItem('refreshToken')
+        const data = await dispatch(requestAccessToken({refreshToken}))
+
+        config.headers.authorization = `Bearer ${data.payload.accessToken}`
+        return axios(config)
+      }
+
+      return Promise.reject(error)
+    },
+  )
+}
 
 const InnerApp = memo(() => {
   const dispatch = useAppDispatch()
   const isLoggined = useAppSelector(state => !!state.user.accessToken)
 
   useEffect(() => {
-    const getRefreshToken = async () => {
-      const refreshToken = await EncryptedStorage.getItem('refreshToken')
-      if (!refreshToken) {
-        SplashScreen.hide()
-        return
-      }
-
-      dispatch(requestAccessToken({refreshToken}))
-        .unwrap()
-        .then(() => {
-          SplashScreen.hide()
-        })
-        .catch(() => {
-          SplashScreen.hide()
-        })
-    }
-    getRefreshToken()
-
-    axios.interceptors.response.use(
-      response => response,
-      async error => {
-        const {
-          config,
-          response: {status},
-        } = error
-
-        // access token 기간 만료시
-        if (status === 419) {
-          console.log('accessToken expired! - accessToken reissue')
-          const refreshToken = await EncryptedStorage.getItem('refreshToken')
-
-          if (refreshToken) {
-            dispatch(requestAccessToken({refreshToken}))
-              .unwrap()
-              .then(response => {
-                config.headers.authorization = `Bearer ${response.accessToken}`
-                return axios(config)
-              })
-          }
-        }
-
-        return Promise.reject(error)
-      },
-    )
+    getRefreshToken(dispatch)
+    axiosInterceptor(dispatch)
   }, [])
 
   return (
