0733d9bf3faf899bf81a6f7e433cceb0f527f9ad
hyorimlee <hl1046@pusan.ac.kr>
2022-05-06 16:04:09 +0900

    [S06P31A302-183] feat:add path profile

--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -18,13 +18,35 @@ profileRouter.get('/', authAccessToken,async (req, res) => {
     }
 })
 
-profileRouter.patch('/', async (req, res) => {
+profileRouter.patch('/',authAccessToken, async (req, res) => {
     try {
-
-        const user = null
-        // 프로필 수정 로직
-
-        return res.status(200).send({ user })
+        let user = await User.findById(req.userId)
+        const {username,nickname,profileImage,description,phone} = req.body
+        if(username){
+            if(typeof username!=="string") return res.status(400).send({err:"username 형식이 잘못되었습니다."})
+            if(await User.findOne({username:username})) return res.status(400).send({err:"이미 존재하는 username입니다"})
+            if(username.length>10) return res.status(400).send({err:"username 길이가 10을 넘습니다"})
+            user.username = username
+        }
+        if(nickname){
+            if(typeof nickname!=="string") return res.status(400).send({err:"nickname 형식이 잘못되었습니다."})
+            if(nickname.length>10) return res.status(400).send({err:"nickname 길이가 10을 넘습니다"})
+            user.nickname = nickname
+        }
+        if(phone){
+            if(typeof phone!=="string") return res.status(400).send({err:"phone 형식이 잘못되었습니다."})
+            user.phone = phone
+        }
+        if(profileImage) {
+            if(typeof profileImage!=="string") return res.status(400).send({err:"profileImage 형식이 잘못되었습니다."})
+            user.profileImage = profileImage
+        }
+        if(description){
+            if(typeof description!=="string") return res.status(400).send({err:"description의 형식이 잘못되었습니다."})
+            user.description = description
+        }
+        await user.save()
+        return res.status(200).send({msg:"DONE",user:user.username })
     } catch (error) {
         console.log(error)
         return res.status(500).send({ err: error.message })
