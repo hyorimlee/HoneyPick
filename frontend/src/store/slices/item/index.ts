a4b4a8eeb561cc0e92d5bda56cbce83efdf37012
yoon <jiyoonhan12@gmail.com>
2022-05-08 04:40:52 +0900

    [S06P31A302-152] feat: itemToCollection

--- frontend/src/store/slices/item/index.ts
+++ frontend/src/store/slices/item/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Config from 'react-native-config'
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
-import {ItemState} from './types'
+import {ItemState, IItemToCollectionParameter} from './types'
 import {RootState} from '../../types'
 
 const initialState = {
@@ -48,12 +48,35 @@ export const saveItem = createAsyncThunk<any, string, {state: RootState}>(
 
 export const getItem = createAsyncThunk<any, string, {state: RootState}>(
   'item/getItem',
-  async (data: string, thunkAPI) => {
+  async (itemId: string, thunkAPI) => {
     try {
       const {accessToken} = thunkAPI.getState().user
       const response = await axios({
         method: 'GET',
-        url: `${Config.API_BASE_URL}/item/${data}`,
+        url: `${Config.API_BASE_URL}/item/${itemId}`,
+        headers: {
+          authorization: `Bearer ${accessToken}`
+        }
+      })
+      return response.data
+    } catch (err: any) {
+      return thunkAPI.rejectWithValue(err.response.data)
+    }
+  }
+)
+
+// originalCollectionId만 있는 경우 삭제
+// collectionID만 있는 경우 추가
+// 둘 다 있는 경우 이동
+export const itemToCollection = createAsyncThunk<any, IItemToCollectionParameter, {state: RootState}>(
+  'item/itemToCollection',
+  async ({itemId, originalCollectionId, collectionId}: IItemToCollectionParameter, thunkAPI) => {
+    try {
+      const {accessToken} = thunkAPI.getState().user
+      const response = await axios({
+        method: 'POST',
+        url: `${Config.API_BASE_URL}/item/${itemId}`,
+        data: {originalCollectionId, collectionId},
         headers: {
           authorization: `Bearer ${accessToken}`
         }
--- frontend/src/store/slices/item/types.ts
+++ frontend/src/store/slices/item/types.ts
@@ -1,21 +1,27 @@
 export interface ItemState {
-  itemId: string,
+  itemId: string
   item: {
-    _id: string,
-    brand?: string,
-    url: string,
-    title?: string,
-    thumbnail?: string,
-    priceBefore?: number,
-    priceAfter?: number,
-    discountRate?: number,
-    stickers: object,
+    _id: string
+    brand?: string
+    url: string
+    title?: string
+    thumbnail?: string
+    priceBefore?: number
+    priceAfter?: number
+    discountRate?: number
+    stickers: object
   },
   review: {
-    _id: string,
-    user: string,
-    item: string,
-    isRecommend: 0 | 1 | 2,
-    stickers: number[],
+    _id: string
+    user: string
+    item: string
+    isRecommend: 0 | 1 | 2
+    stickers: number[]
   } | null
+}
+
+export interface IItemToCollectionParameter {
+  itemId: string
+  originalCollectionId?: string
+  collectionId?: string
 }
