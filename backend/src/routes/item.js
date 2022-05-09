20511938ae7e788058002bf5a2d03b201358e1ee
sosin <svstar94@naver.com>
2022-05-09 12:31:45 +0900

    [S06P31A302-190] feat: add image logic

--- backend/src/routes/item.js
+++ backend/src/routes/item.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose')
 const { isValidObjectId, Types: { ObjectId } } = require('mongoose')
 const { Item, Review, Collection } = require('../models')
 
+const { v4: uuid } = require("uuid")
+const mime = require("mime-types")
+
 const { authAccessToken } = require('./auth')
+const { getSignedUrl } = require('../aws')
 
 const axios = require('axios')
 
@@ -13,38 +17,43 @@ CRAWLING_SERVER_URL = process.env.CRAWLING_SERVER_URL
 itemRouter.use('/:itemId/review', require('./review'))
 
 itemRouter.post('/', authAccessToken, async (req, res) => {
-    // 아이템 생성 로직
     try {
         const { url } = req.body
         if(typeof url !== 'string') return res.status(400).send({ err: "url is required" })
         
         var item = await Item.findOne({ url })
 
-        // item이 존재하지않거나, 크롤링이 진행되지 않았을 경우
-        // 크롤링이 진행되지 않았을 경우는 나중에 제거하거나 updatedAt을 통해 체크 후 크롤링 요청 필요로 변경
-        if(!item || !item.title) {
-            // mongodb에 데이터 생성
-            if(!item){
-                item = new Item({ ...req.body })
-                await item.save()
-            }
-            // 크롤링 서버 요청 (Return 필요없음)
-            test = false
-            if(!test) {
-                axios({
-                    method: 'post',
-                    url: `${CRAWLING_SERVER_URL}/item`,
-                    data: {
-                        url: url,
-                        item_id: item._id,
-                    }
-                }).then(() => {
-                    console.log('item crawl success')
-                }).catch(({response}) => {
-                    console.log(response)
-                })
-            }
+        var needCrawl = false
+        
+        if(!item) {
+            item = new Item({ ...req.body })
+            await item.save()
+            needCrawl = true
         }
+
+        if(item.updatedAt) {
+            const nowDate = new Date()
+            const crawlDate = new Date(item.updatedAt)
+            const hourDiff = (nowDate.getTime() - crawlDate.getTime()) / (1000*60*60)
+            if(hourDiff > 24) needCrawl = true
+        }
+
+        if(needCrawl) {
+            axios({
+                method: 'post',
+                url: `${CRAWLING_SERVER_URL}/item`,
+                headers: { user_id: req.userId },
+                data: {
+                    url: url,
+                    item_id: item._id,
+                }
+            }).then(() => {
+                console.log('item crawl success')
+            }).catch(({response}) => {
+                console.log(response)
+            })
+        }
+
         return res.status(201).send({ _id: item._id })
     } catch (error) {
         console.log(error)
@@ -55,8 +64,9 @@ itemRouter.post('/', authAccessToken, async (req, res) => {
 itemRouter.get('/:itemId', authAccessToken, async (req, res) => {
     try {
         const { itemId } = req.params
-        // query에 userId가 없을 경우 본인의 리뷰 가져오기
         var { userId } = req.query
+        
+        // query에 userId가 없을 경우 본인의 리뷰 가져오기
         if(!userId) userId = req.userId
         
         if(!isValidObjectId(itemId)) return res.status(400).send({ err: "잘못된 itemId" })
@@ -108,6 +118,26 @@ itemRouter.patch('/:itemId', authAccessToken, async (req, res) => {
         await Promise.all(promises)
 
         return res.status(200).send({ message: 'success' })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send({ err: error.message })
+    }
+})
+
+itemRouter.post('/:itemId/presigned', async(req, res) => {
+    try {
+        const { itemId } = req.params
+        if(!isValidObjectId(itemId)) return res.status(400).send({ err: "잘못된 itemId" })
+
+        const { contentType } = req.body
+        
+        const imageKey = `${uuid()}.${mime.extension(contentType) ? mime.extension(contentType) : 'jpg'}`
+        const key = `raw/${imageKey}`
+        const presigned = await getSignedUrl({ key })
+
+        await Item.findByIdAndUpdate(itemId, { $set: { thumbnail: imageKey } })
+
+        return res.status(200).send({ imageKey, presigned })
     } catch (error) {
         console.log(error)
         return res.status(500).send({ err: error.message })
