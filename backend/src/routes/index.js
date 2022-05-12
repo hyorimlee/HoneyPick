b1cdf392a98d94e394ac3eddc6b4b0e91f429d8a
hyorimlee <hl1046@pusan.ac.kr>
2022-05-12 13:25:32 +0900

    [S06P31A302-197] fix:fix conflict

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -8,9 +8,6 @@ router.use('/vote', require('./vote'))
 router.use('/phone', require('./phone'))
 router.use('/profile',require('./profile'))
 router.use('/like', require('./like'))
-<<<<<<< HEAD
 router.use('/search', require('./search'))
-=======
 router.use('/recommend', require('./recommend'))
->>>>>>> 67dbb8c3187b5f2ae74bbb6ab55a338803227d58
 module.exports = router
