e7637d5feafad18fa7ee0bf1e05cfb8cc762a860
yoon <jiyoonhan12@gmail.com>
2022-05-13 10:17:59 +0900

    [S06P31A302-198] feat: follow styling

--- frontend/src/components/flatList/followList/styles.ts
+++ frontend/src/components/flatList/followList/styles.ts
@@ -3,11 +3,34 @@ import styled from 'styled-components/native'
 export const CustomFlatList = styled.FlatList`
   padding: 10px;
   height: 300px;
-  background-color: red;
 `
 
 export const HorizontalContainer = styled.View`
   flex-direction: row;
   justify-content: space-between;
-  margin: 10px;
+  margin-vertical: 10px;
+  margin-horizontal: 20px;
+`
+
+export const ProfileImage = styled.Image`
+  width: 48px;
+  height: 48px;
+  border-radius: 100px;
+  resize-mode: contain;
+  background-color: black;
+`
+
+export const ButtonContainer = styled.View`
+  position: absolute;
+  right: 0;
+`
+
+export const Nickname = styled.Text`
+  font-weight: 600;
+  color: #000000;
+`
+
+export const Description = styled.Text`
+  font-size: 12px;
+  color: #000000;
 `
