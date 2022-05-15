25e008343a5f4a4ebdf9dc3a7b445501f31d41a0
yoon <jiyoonhan12@gmail.com>
2022-05-16 02:00:55 +0900

    [S06P31A302-209] feat: event asyncThunk

--- frontend/src/store/slices/event/index.ts
+++ frontend/src/store/slices/event/index.ts
@@ -1,10 +1,25 @@
 import {createSlice} from '@reduxjs/toolkit'
-import {EventState} from './types'
-import {
-} from './asyncThunk'
+import {EventInitialState} from './types'
 import {RootState, useAppSelector} from '~/store/types'
 
-const initialState: EventState = {
+import {saveEvent, getEventList, getEvent, editEvent} from './asyncThunk'
+
+// 추후 BE에서 user 수정해주면 변경해야 함
+const initialState: EventInitialState = {
+  event: {
+    user: {
+      _id: '',
+      username: '',
+    },
+    title: '',
+    isPublic: true,
+    _id: '',
+    items: [],
+    createdAt: '',
+    updatedAt: '',
+    __v: 0
+  },
+  eventList: []
 }
 
 const eventSlice = createSlice({
@@ -14,6 +29,18 @@ const eventSlice = createSlice({
   },
   extraReducers: builder => {
     builder
+      .addCase(saveEvent.fulfilled, (state, action) => {
+        state.event = action.payload.event
+      })
+      .addCase(getEventList.fulfilled, (state, action) => {
+        state.eventList = action.payload.events
+      })
+      .addCase(getEvent.fulfilled, (state, action) => {
+        state.event = action.payload.event
+      })
+      .addCase(editEvent.fulfilled, (state, action) => {
+        state.event = action.payload.event
+      })
   },
 })
 
