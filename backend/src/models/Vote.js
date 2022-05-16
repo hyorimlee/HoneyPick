d6a887ab44d6c0af3d855ad08a0990fb43d832cb
itzmeee <itzmeee@naver.com>
2022-05-16 10:12:31 +0900

    [S06P31A302-236] update: changed field name from collectionId to targetId to process both collection and event

--- backend/src/models/Vote.js
+++ backend/src/models/Vote.js
@@ -2,7 +2,7 @@ const { Schema, model, Types: { ObjectId } } = require('mongoose')
 
 const VoteSchema = new Schema(
   {
-    collectionId: { type: ObjectId, required: true, ref: 'collection' },
+    targetId: { type: ObjectId, required: true},
     title: { type: String, required: false },
     result: [{
       _id: { type: ObjectId, required: true, ref: 'item' },
