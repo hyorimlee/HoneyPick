903735bd328508551881646a45b840a6bc51da91
sosin <svstar94@naver.com>
2022-05-16 10:25:03 +0900

    feat: add username, nickname to Review

--- backend/src/models/Review.js
+++ backend/src/models/Review.js
@@ -1,7 +1,11 @@
 const { Schema, model, Types: { ObjectId } } = require('mongoose')
 
 const ReviewSchema = new Schema({
-    user: { type: ObjectId, required: true, ref: 'user' },
+    user: {
+        _id: { type: ObjectId, required: true, ref: 'user' },
+        username: { type: String, required: true },
+        nickname: { type: String, required: true }
+    },
     item: { type: ObjectId, required: true, ref: 'item' },
     isRecommend: { type: Number, required: true, default: 0 },
     stickers: { type: Array, required: true, default: []}
