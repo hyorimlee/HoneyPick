d7548c202cee4056938ad123d7d723f582cfa9c3
itzmeee <itzmeee@naver.com>
2022-05-17 15:24:30 +0900

    [S06P31A302-239] update: schema update to support giving details on items

--- backend/src/models/Vote.js
+++ backend/src/models/Vote.js
@@ -7,6 +7,10 @@ const VoteSchema = new Schema(
     title: { type: String, required: false },
     result: [{
       _id: { type: ObjectId, required: true, ref: 'item' },
+      title: { type: String, required: true },
+      thumbnail: { type: String, required: true },
+      priceBefore: { type: Number, required: true },
+      priceAfter: { type: Number, required: false },
       count: { type: Number, required: true, default: 0 }
     }],
     isPublic: { type: Boolean, required: true, default: true },
