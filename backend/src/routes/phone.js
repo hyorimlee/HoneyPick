c8dd2055cfc980c8e7e2f2ad5f7a5be83895e0c9
sosin <svstar94@naver.com>
2022-05-18 15:10:12 +0900

    feat: add sending sms

--- backend/src/routes/phone.js
+++ backend/src/routes/phone.js
@@ -4,18 +4,27 @@ const mongoose = require('mongoose')
 const { isValidObjectId } = require('mongoose')
 const { Phone } = require('../models')
 
+const { send } = require('../sms')
+
 phoneRouter.post('/', async (req, res) => {
     try {
         const { phoneNumber } = req.body
         if(typeof phoneNumber !== 'string') return res.status(400).send({ err: "핸드폰 번호를 입력해주세요." })
+        
         // 폰번호 검증 01000000000
 
         const verificationCode = Math.floor(Math.random()*1000000).toString().padStart(6, '0')
         
-        // 문자 발송 로직
-
         const phone = new Phone({ phoneNumber, verificationCode })
         await phone.save()
+
+        // 테스트
+        // phone.verificationCode = undefined
+
+        // // 문자 발송
+        // send(phoneNumber.replace('-', ''), verificationCode)
+        //     .then(res => console.log(res))
+        //     .catch(err => console.log(err))
 
         return res.status(201).send({ phone })
     } catch (error) {
