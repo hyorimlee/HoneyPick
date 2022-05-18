fccf0ab3cdbe20248f412db31673fd581e168308
sosin <svstar94@naver.com>
2022-05-18 10:20:02 +0900

    [S06P31A302-272] fix: search indexing

--- backend/src/models/Item.js
+++ backend/src/models/Item.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose')
 const ItemSchema = new Schema({
     url: { type: String, required: true },
     title: { type: String , required: false },
-    thumbnail: { type: String, required: false },
+    thumbnail: { type: String, required: false, default: 'honeypick.png' },
     priceBefore: { type: Number, required: false },
     priceAfter: { type: Number, required: false },
     discountRate: { type: Number, required: false },
@@ -13,6 +13,8 @@ const ItemSchema = new Schema({
 
 }, { timestamps: true })
 
+ItemSchema.index({ title: 'text' })
 const Item = model('item', ItemSchema)
+Item.createIndexes()
 
 module.exports = { Item, ItemSchema }
